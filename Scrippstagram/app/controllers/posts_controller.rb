54b1ec53a7ca0c24065d0b9c6e81be1915bef997
Wynne Becker <wbecker@cs.hmc.edu>
2018-02-10 16:10:28 -0800

    Added create and new actions

--- Scrippstagram/app/controllers/posts_controller.rb
+++ Scrippstagram/app/controllers/posts_controller.rb
@@ -4,4 +4,19 @@ def index
 
 end
 
+def new
+    @post = Post.new
+end
+
+def create
+    @post = Post.create(post_params)
+    redirect_to posts_path
+end
+
+private
+
+def post_params
+  params.require(:post).permit(:image, :caption)
+end
+
 end
