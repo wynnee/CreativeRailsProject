26b14c2e0737d5ba06859d00a460b5eae9c6c649
Diana Arreola <arreoladiana11@gmail.com>
2018-02-12 22:38:25 -0800

    implemented edit and delete post

--- Scrippstagram/app/controllers/posts_controller.rb
+++ Scrippstagram/app/controllers/posts_controller.rb
@@ -1,7 +1,8 @@
 class PostsController < ApplicationController
+    before_action :set_post, only: [:show, :edit, :update, :destroy]
 
 def index 
-    @posts = Post.all
+    @post = Post.all
 end
 
 def new
@@ -9,13 +10,32 @@ def new
 end
 
 def create
-    @post = Post.create(post_params)
-    redirect_to posts_path
+    if @post = Post.create(post_params)
+        redirect_to posts_path
+    else
+        render :new
+    end
 end
 
 def show
-    @post = Post.find(params[:id])
 end
+
+def edit
+end
+
+def update
+    if @post.update(post_params)
+        redirect_to(post_path(@post))
+    else
+        render :edit
+    end
+end 
+
+def destroy
+    @post.destroy
+    redirect_to posts_path
+end
+
 
 private
 
@@ -23,14 +43,8 @@ def post_params
   params.require(:post).permit(:image, :caption)
 end
 
-def edit
+def set_post
     @post = Post.find(params[:id])
 end
-
-def update
-    @post = Post.find(params[:id])
-    @post.update(post_params)
-    redirect_to(post_path(@post))
-end 
 
 end
